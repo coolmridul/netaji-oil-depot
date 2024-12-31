33f92e54076c9b4f883a2288f23364263c08c87c
coolmridul <mridulagarwal8@gmail.com>
2024-12-31 13:22:46 +0530

    0.2.1

--- send_whatsapp.py
+++ send_whatsapp.py
@@ -4,7 +4,7 @@ url_whatsapp = "https://api.wassenger.com/v1/messages"
 
 headers_whatsapp = {
     "Content-Type": "application/json",
-    "Token": "34ad67d75b6185d117c6f7134ed2fbb70e0afbfb7c5783c791bcfda7a4512161255ce04177a85dab"
+    "Token": "76814737bd1d2336bf151cc1512466cc3f33be295acb5ebb2257db7f36520381aa0b9ae6f6bfefff"
 }
 
 
